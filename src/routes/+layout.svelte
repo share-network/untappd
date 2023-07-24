2819654f135058997becc2a2653dfdb505a42c5b
Michael Nero <michaelnero@outlook.com>
2023-07-24 18:52:46 +0000

    adding analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,10 @@
 <script>
   import "../app.postcss";
+  import { dev } from "$app/environment";
 </script>
 
 <slot />
+
+{#if !dev}
+<script defer src='https://static.cloudflareinsights.com/beacon.min.js' data-cf-beacon='{"{"}"token": "6e633c6ed03340bb93e852a2ca928ea4"{"}"}'></script>
+{/if}
