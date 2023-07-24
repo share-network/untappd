f7b5468f69ae5a64ddccb98fef46a62514bfb316
Michael Nero <michaelnero@outlook.com>
2023-07-24 18:16:15 +0000

    - adding d1 binding - some formatting

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -12,17 +12,17 @@ const config = {
     extend: {
       colors: {
         "untappd-dark": {
-          "50": "#e8e9eb",
-          "100": "#d2d3d6",
-          "200": "#a5a7ad",
-          "300": "#777b84",
-          "400": "#4a4f5b",
-          "500": "#1d2332",
-          "600": "#171c28",
-          "700": "#11151e",
-          "800": "#0c0e14",
-          "900": "#06070a",
-          "950": "#030305",
+          50: "#e8e9eb",
+          100: "#d2d3d6",
+          200: "#a5a7ad",
+          300: "#777b84",
+          400: "#4a4f5b",
+          500: "#1d2332",
+          600: "#171c28",
+          700: "#11151e",
+          800: "#0c0e14",
+          900: "#06070a",
+          950: "#030305"
         },
         "untappd-dark-blue": "#1c4aba",
         "untappd-blue": "#039aff",
@@ -32,7 +32,7 @@ const config = {
         "untappd-pink": "#f9bbc6",
         "untappd-alice-blue": "#f2f9ff",
         "untappd-white-smoke": "#f8fafc",
-        "untappd-gray": "#6b738a",
+        "untappd-gray": "#6b738a"
       }
     }
   },
