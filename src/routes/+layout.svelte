a85c7006629bac977d8e2ff851dda059a1d03079
Michael Nero <michaelnero@outlook.com>
2023-07-24 18:59:12 +0000

    fixing analytics attribute

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,5 +6,5 @@
 <slot />
 
 {#if !dev}
-<script defer src='https://static.cloudflareinsights.com/beacon.min.js' data-cf-beacon='{"{"}"token": "6e633c6ed03340bb93e852a2ca928ea4"{"}"}'></script>
+{@html "<script defer src='https://static.cloudflareinsights.com/beacon.min.js' data-cf-beacon='{\"token\": \"6e633c6ed03340bb93e852a2ca928ea4\"}'></script>"}
 {/if}
