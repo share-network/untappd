84176e3e924385c01dc11abff3d13e82c8c63713
Michael Nero <michaelnero@outlook.com>
2023-07-24 19:02:07 +0000

    adding analytics to app.html

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,5 @@
 <script>
   import "../app.postcss";
-  import { dev } from "$app/environment";
 </script>
 
 <slot />
-
-{#if !dev}
-{@html "<script defer src='https://static.cloudflareinsights.com/beacon.min.js' data-cf-beacon='{\"token\": \"6e633c6ed03340bb93e852a2ca928ea4\"}'></script>"}
-{/if}
